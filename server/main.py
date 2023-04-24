c7f7c6e8cbd2d399642dc819a69516ccbfda2623
Filip Domovic <domovic.filip@gmail.com>
2023-04-24 18:55:27 +0200

    little changes to fastapi server to look prettier

--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ import uvicorn
 from typing import Union, List
 import utility
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 
 
 app = FastAPI()
@@ -17,8 +18,10 @@ async def upload_image_file(image: UploadFile = File(...)):
     img_bytes = await image.read()
     np_img  = np.frombuffer(img_bytes, np.uint8)
     img = cv2.imdecode(np_img , cv2.IMREAD_COLOR)
+    
+    result = utility.process_n_calc(img)
 
-    return {"result" : str(utility.process_n_calc(img))}
+    return JSONResponse(content={"result": str(result)})
 
 if __name__=="__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
